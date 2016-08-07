8e893dbfce19c894acd9b79588af237fc16441ad
Tasmitt <tasmitt@gmail.com>
2016-08-07 23:53:20 +0300

    Cats project init

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * This class is really helpful.
+ * You don't actually know how much.
+ * Neither do I.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	//Some cats say this.
+        System.out.println("Purrfect");
+        /*
+           Or even
+           Some other stuff
+           I don't know exactly
+            */
+    }
+}
